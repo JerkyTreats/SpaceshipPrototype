11c24920aba5cbf7cc8342a3216f4a2be5905874
JerkyTreats <justinfnearing@gmail.com>
2017-05-01 14:26:07 -0700

    Pull SpaceshipPrototype out of EndlessHorizons

---
+++ Assets/Scripts/Character/NPC/NPC.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Characters;
+using Needs;
+
+namespace NPC
+{
+	//"Non Player Character"
+    public class NPC : Character
+    {
+    	public Planner Planner;
+    	
+		new void Start()
+		{
+			base.Start();
+			Planner = gameObject.GetComponent<Planner>();
+			Planner.InvokeDetermineHighestGoal();
+		}
+    }
+}
